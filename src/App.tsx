4a001fc60d03942a1ffd0e5c57a600a8d1b7ace7
jeff0518 <cycivs07@icloud.com>
2024-03-07 11:38:02 +0800

    feat: 初始化

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import logo from "../public/logo.svg";
+import "../src/styles/globals.scss";
+
+function App() {
+  return (
+    <>
+      <img src={logo} className="logo" alt="Vite logo" />
+    </>
+  );
+}
+
+export default App;
