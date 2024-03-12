eea02e5c88c59dc9cbca47c6cea902cfb6d641f1
jeff0518 <cycivs07@icloud.com>
2024-03-12 11:33:15 +0800

    feat:add 點擊圖片跳出完整圖片

--- src/components/patient/RecordCard.tsx
+++ src/components/patient/RecordCard.tsx
@@ -2,6 +2,8 @@ import { useContext } from "react";
 import { BiTrashAlt } from "react-icons/bi";
 
 import { RecordFakeDataContext } from "../../context/recordFakeData";
+import { PreviewContext } from "../../context/previewContext";
+import UserProgressContext from "../../context/UserProgressContext";
 import ButtonUI from "../UI/ButtonUI";
 import style from "./RecordCard.module.scss";
 
@@ -12,14 +14,21 @@ interface RecordProps {
 }
 function RecordCard({ imageId, imageUrl, recordNumber }: RecordProps) {
   const recordFakeCtx = useContext(RecordFakeDataContext);
+  const userProgressCtx = useContext(UserProgressContext);
+  const previewCtx = useContext(PreviewContext);
 
   const deledImageHandler = () => {
     recordFakeCtx?.deledPhoto({ recordNumber, imageId });
   };
+
+  const clickImageHandler = () => {
+    previewCtx?.addImage(imageUrl);
+    userProgressCtx.showPreview();
+  };
   return (
     <li className={style.recordCard_container}>
       <div className={style.image}>
-        <img src={imageUrl} alt="圖片" />
+        <img src={imageUrl} alt="圖片" onClick={clickImageHandler} />
         <div className={style.trash}>
           <ButtonUI btnStyle="btn__card" onClick={deledImageHandler}>
             <BiTrashAlt size={30} />
